d3d59df0f622f8c2d04ccc1057cc3bc9d7df6f3e
Kartikey Agrawal <kartikey04.05@gmail.com>
2017-10-26 00:42:48 +0530

    First version to edit values in sheet

---
+++ sheetsTest.py
@@ -0,0 +1,21 @@
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+GDOCS_OAUTH_JSON = 'raspberryTest.json'
+
+GDOCS_SPREADSHEET_NAME = 'PythonTest'
+
+scope = ['https://spreadsheets.google.com/feeds']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(GDOCS_OAUTH_JSON, scope)
+print 'received'
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open(GDOCS_SPREADSHEET_NAME).sheet1
+
+value = wks.acell('B1').value
+print value
+
