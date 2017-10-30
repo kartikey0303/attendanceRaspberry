573a01f1a457c168a0b135a4de1cd8173022b9ea
Kartikey Agrawal <kartikey04.05@gmail.com>
2017-10-30 18:20:59 +0530

    Added search function

--- sheetsTest.py
+++ sheetsTest.py
@@ -1,6 +1,18 @@
-import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+
+
+def searchRFID(name, wks):
+    '''returns the RFID of the person whose name is given'''
+    print wks.row_count
+    for k in range(2, wks.row_count):
+        row = wks.row_values(k)[0:3]
+        if row[0] == name:
+            return row[2]
+
+
+def searchName(rfid):
+    '''returns the name of person whose RFID is given'''
 
 
 GDOCS_OAUTH_JSON = 'raspberryTest.json'
@@ -20,6 +32,9 @@ name  = [item for item in wks.col_values(1) if item]
 
 for item in name:
     print item
+
+RFID = searchRFID("Kartikey", wks)
+print RFID
 #value = wks.acell('A2').value
 #print value
 
